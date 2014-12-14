835ed63b61b4886a110393412493fcf88b717801
Mohammed Hammoud <mohammed@iktw.se>
2014-12-14 13:00:47 +0100

    Added DEBUG to views/index render context

--- project/core/views.py
+++ project/core/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-
+from django.conf import settings
 
 def index(request):
     template = 'core/index.html'
-    return render(request, template)
+    context = {'DEBUG': settings.DEBUG}
+    return render(request, template, context)
