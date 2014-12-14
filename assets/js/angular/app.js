d6e667af5b54d76c4bee4bbdc44f0810f708e4b7
Mohammed Hammoud <mohammed@iktw.se>
2014-12-14 12:58:53 +0100

    Added pascalprecht.translate to module

--- assets/js/angular/app.js
+++ assets/js/angular/app.js
@@ -1,6 +1,7 @@
-angular.module('mainModule', ['restangular', 'ui.router', 'ui.bootstrap'])
-    .config(['$httpProvider', 'RestangularProvider', function ($httpProvider, RestangularProvider) {
+angular.module('mainModule', ['restangular', 'ui.router', 'ui.bootstrap', 'pascalprecht.translate'])
+    .config(['$httpProvider', 'RestangularProvider', '$translateProvider', function ($httpProvider, RestangularProvider, $translateProvider) {
         $httpProvider.defaults.xsrfCookieName = 'csrftoken';
         $httpProvider.defaults.xsrfHeaderName = 'X-CSRFToken';
         RestangularProvider.setRequestSuffix('/');
+        $translateProvider.preferredLanguage('en');
     }]);
