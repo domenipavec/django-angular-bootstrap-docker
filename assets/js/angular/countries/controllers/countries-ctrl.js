2c9693ee4dff310b97c5b0d5c267f314d6de67ba
Mohammed Hammoud <mohammed@iktw.se>
2015-01-28 23:00:57 +0100

    Removed country list

--- assets/js/angular/countries/controllers/countries-ctrl.js
+++
@@ -1,4 +0,0 @@
-angular.module('mainModule')
-    .controller('CountriesCtrl', ['$scope', 'CountriesFactory', function ($scope, CountriesFactory) {
-        $scope.countries = CountriesFactory.getListOfEurope();
-    }]);
--- assets/js/angular/countries/factories/countries-factory.js
+++
@@ -1,8 +0,0 @@
-angular.module('mainModule')
-    .factory('CountriesFactory', ['Restangular', function (Restangular) {
-        return {
-            getListOfEurope: function () {
-                return Restangular.oneUrl('europe', 'http://restcountries.eu/rest/v1/region/europe').getList().$object;
-            }
-        }
-    }]);
