d643bd5cde803669d8346072ef99496c3cb32b96
Mohammed Hammoud <mohammed@iktw.se>
2015-01-28 23:02:12 +0100

    Removed unnecessary translation keys

--- assets/js/angular/translate/translations/en.js
+++ assets/js/angular/translate/translations/en.js
@@ -1,13 +1,8 @@
 angular.module('mainModule')
     .config(['$translateProvider', function ($translateProvider) {
         $translateProvider.translations('en', {
-            lets_list_countries_in_europe: 'Let\'s list countries in Europe.',
-            title_country: 'Country',
-            title_native_name: 'Native name',
-            title_capital: 'Capital',
-            title_currencies: 'Currencies',
-            title_languages: 'Languages',
             button_text_en: 'English',
-            button_text_sv: 'Swedish'
+            button_text_sv: 'Swedish',
+            label_which_language_do_you_prefer: 'Which language to you prefer?'
         });
     }]);
--- assets/js/angular/translate/translations/sv.js
+++ assets/js/angular/translate/translations/sv.js
@@ -1,13 +1,8 @@
 angular.module('mainModule')
     .config(['$translateProvider', function ($translateProvider) {
         $translateProvider.translations('sv', {
-            lets_list_countries_in_europe: 'Låt oss lista alla länder i Europa',
-            title_country: 'Land',
-            title_native_name: 'Nativt namn',
-            title_capital: 'Huvudstad',
-            title_currencies: 'Valuta',
-            title_languages: 'Språk',
             button_text_en: 'Engelska',
-            button_text_sv: 'Svenska'
+            button_text_sv: 'Svenska',
+            label_which_language_do_you_prefer: 'Vilket språk föredrar du?'
         });
     }]);
