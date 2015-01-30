854f31eb4fefceed9ead114404a37eca6fce3df9
Mohammed Hammoud <mohammed@iktw.se>
2015-01-30 07:29:40 +0100

    Added 404.html template & if route hits otherwise redirect to /404.html

---
+++ assets/js/angular/base/routes.js
@@ -0,0 +1,16 @@
+angular.module('mainModule')
+    .config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $urlRouterProvider) {
+        $urlRouterProvider.otherwise("/404.html");
+        $stateProvider
+            .state('404', {
+                url: "/404.html",
+                views: {
+                    "FullContentView": {
+                        templateUrl: 'errors/404.html'
+                    }
+                },
+                onEnter: ['PageService', function (PageService) {
+                    PageService.output._404MetaData();
+                }]
+            })
+    }]);
