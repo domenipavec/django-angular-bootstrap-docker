3c7f812d7c22c21e6cda4a0ca3fada8526b1854b
Mohammed Hammoud <mohammed@iktw.se>
2015-01-30 07:21:41 +0100

    Html5Mode on

--- assets/js/angular/config.js
+++ assets/js/angular/config.js
@@ -1,10 +1,8 @@
 angular.module('mainModule')
-    .config(['$httpProvider', 'RestangularProvider', '$translateProvider', '$locationProvider',
-        function ($httpProvider, RestangularProvider, $translateProvider, $locationProvider) {
+    .config(function ($httpProvider, RestangularProvider, $translateProvider, $locationProvider) {
             $httpProvider.defaults.xsrfCookieName = 'csrftoken';
             $httpProvider.defaults.xsrfHeaderName = 'X-CSRFToken';
             RestangularProvider.setRequestSuffix('/');
-            $translateProvider.preferredLanguage('sv');
-            $locationProvider.hashPrefix('!');
-//            $locationProvider.html5Mode(true);
-        }]);
+            $translateProvider.preferredLanguage('en');
+            $locationProvider.html5Mode(true);
+        });
