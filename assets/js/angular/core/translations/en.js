a56b74152cb5c35c3aa3b57600cc4a04a4d170b1
Mohammed Hammoud <mohammed@iktw.se>
2015-01-30 07:22:45 +0100

    Add new language keys

--- assets/js/angular/core/translations/en.js
+++ assets/js/angular/core/translations/en.js
@@ -3,6 +3,14 @@ angular.module('mainModule')
         $translateProvider.translations('en', {
             button_text_en: 'English',
             button_text_sv: 'Swedish',
-            label_which_language_do_you_prefer: 'Which language do you prefer?'
+            label_which_language_do_you_prefer: 'Which language do you prefer?',
+
+            meta_title_404: '404 - Page Not Found',
+            meta_keywords_404: 'Error, 404',
+            meta_description_404: 'The page you\'r trying to access does not exist.',
+
+            meta_title_core: 'Django AngularJs Boilerplate',
+            meta_description_core: 'This is a boilerplate which includes django angularjs bootstrap and more.',
+            meta_keywords_core: 'Django, angularjs, bootstrap, gulp, bower.',
         });
     }]);
