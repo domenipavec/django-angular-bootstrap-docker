9557fa36046c8747dc7245b34f1bb4af948e7de8
Rafal Mahdi <rafalmahdi@Rafals-MacBook-Air.local>
2015-02-08 14:32:10 +0100

    Fix uglify error

--- assets/js/angular/config.js
+++ assets/js/angular/config.js
@@ -1,8 +1,9 @@
 angular.module('mainModule')
-    .config(function ($httpProvider, RestangularProvider, $translateProvider, $locationProvider) {
-            $httpProvider.defaults.xsrfCookieName = 'csrftoken';
-            $httpProvider.defaults.xsrfHeaderName = 'X-CSRFToken';
-            RestangularProvider.setRequestSuffix('/');
-            $translateProvider.preferredLanguage('en');
-            $locationProvider.html5Mode(true);
-        });
+.config(['$httpProvider', 'RestangularProvider', '$translateProvider', '$locationProvider',
+	function ($httpProvider, RestangularProvider, $translateProvider, $locationProvider) {
+		$httpProvider.defaults.xsrfCookieName = 'csrftoken';
+		$httpProvider.defaults.xsrfHeaderName = 'X-CSRFToken';
+		RestangularProvider.setRequestSuffix('/');
+		$translateProvider.preferredLanguage('en');
+		$locationProvider.html5Mode(true);
+	}]);
